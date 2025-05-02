30ad0edc9b578e6b58c799b12f0516cdfd1960e1
Nikhil-CT <nikhil.n@cloudthat.com>
2025-05-02 17:16:21 +0530

    Update helm.tf

--- New-OCO/Terraform/modules/helm/helm.tf
+++ New-OCO/Terraform/modules/helm/helm.tf
@@ -42,18 +42,18 @@ resource "null_resource" "deploy_demo" {
 }
 
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-locals {
-  dockerconfigjson = jsonencode({
-    auths = {
-      "https://index.docker.io/v1/" = {
-        username = var.dockerhub_username
-        password = var.dockerhub_password
-      }
-    }
-  })
+# locals {
+#   dockerconfigjson = jsonencode({
+#     auths = {
+#       "https://index.docker.io/v1/" = {
+#         username = var.dockerhub_username
+#         password = var.dockerhub_password
+#       }
+#     }
+#   })
 
-  encoded_dockerconfigjson = base64encode(local.dockerconfigjson)
-}
+#   encoded_dockerconfigjson = base64encode(local.dockerconfigjson)
+# }
 
 
 resource "helm_release" "obs" {
@@ -72,16 +72,16 @@ resource "helm_release" "obs" {
   # }
 
   # Set the Docker Hub pull secret as sensitive
-  set_sensitive {
-    name  = "imagePullSecrets[0].name"
-    value = "dockerhub-secret"  # Name of the secret in Kubernetes
-  }
+  # set_sensitive {
+  #   name  = "imagePullSecrets[0].name"
+  #   value = "dockerhub-secret"  # Name of the secret in Kubernetes
+  # }
 
   # Pass the base64-encoded .dockerconfigjson to the Helm chart (assuming the Helm chart is expecting it)
-  set_sensitive {
-    name  = "imagePullSecrets[0].dockerconfigjson"
-    value = local.encoded_dockerconfigjson
-  }
+  # set_sensitive {
+  #   name  = "imagePullSecrets[0].dockerconfigjson"
+  #   value = local.encoded_dockerconfigjson
+  # }
 
   # Replicate the -f flags from your helm command. Use the repo_root variable.
   values = [
